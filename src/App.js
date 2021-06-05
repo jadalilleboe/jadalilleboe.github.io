22c628bd9473894ef6c11ae8862705fdb48a482a
Jada Lilleboe <lilleboejada@gmail.com>
2021-06-05 14:49:47 -0500

    adds css styling for home page

--- src/App.js
+++ src/App.js
@@ -5,11 +5,25 @@ import './App.css';
 
 const Home = () => {
   return(
-    <div>
+    <div className="home">
+      <nav>
+        <ul>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
       <h1>Hi! I'm Jada Lilleboe.</h1>
       <h2>I'm a computer science student at the University of Minnesota Twin-Cities.</h2>
       <img src={jadaPic} alt="Jada Lilleboe" width={300} height={400}/>
     </div>
+  )
+}
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2021 by Jada Lilleboe.</p>
+    </footer>
   )
 }
 
@@ -17,6 +31,7 @@ const App = () => {
   return(
     <div>
       <Home />
+      <Footer />
     </div>
   )
 }
