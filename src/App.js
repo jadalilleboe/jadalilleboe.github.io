248e2fffaba4000901bf954927a39c847f0a3b02
Jada Lilleboe <lilleboejada@gmail.com>
2021-06-05 12:33:28 -0500

    adds ghpage support

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React from 'react'
+import jadaPic from './img/jadapic.jpeg'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  return(
+    <div>
+      <h1>Hi! I'm Jada Lilleboe.</h1>
+      <h2>I'm a computer science student at the University of Minnesota Twin-Cities.</h2>
+      <image src={jadaPic} alt="Picture of Jada Lilleboe"></image>
     </div>
-  );
+  )
 }
 
 export default App;
