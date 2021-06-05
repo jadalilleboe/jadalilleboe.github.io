cb2546b2f305cf044a8f86cf32e2930dd0d6733f
Jada Lilleboe <lilleboejada@gmail.com>
2021-06-05 14:30:17 -0500

    component rendering

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import jadaPic from './img/jadapic.jpeg'
+import { NavLink, Switch, Route } from 'react-router-dom'
 import './App.css';
 
-const App = () => {
+const Home = () => {
   return(
     <div>
       <h1>Hi! I'm Jada Lilleboe.</h1>
       <h2>I'm a computer science student at the University of Minnesota Twin-Cities.</h2>
       <img src={jadaPic} alt="Jada Lilleboe" width={300} height={400}/>
+    </div>
+  )
+}
+
+const App = () => {
+  return(
+    <div>
+      <Home />
     </div>
   )
 }
