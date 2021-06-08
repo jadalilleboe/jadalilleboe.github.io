df5fcc542771cd3d3edddc6f637d9dee7581023b
Jada Lilleboe <lilleboejada@gmail.com>
2021-06-08 13:25:02 -0500

    responsive layout for homepage

--- src/App.js
+++ src/App.js
@@ -4,31 +4,34 @@ import { NavLink, Switch, Route } from 'react-router-dom'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Blog</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
+
 const Home = () => {
   return(
-    <div className="home-wrapper">
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Blog</li>
-          <li>Projects</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-      <div className="home-content">
-        <div className="home-left left-grid-item">
-          <h1>Hi! I'm Jada Lilleboe.</h1>
-          <h2>I'm a computer science student at the University of Minnesota Twin-Cities.</h2>
-          <p>Feel free to explore this site to learn more about me!</p>
-        </div>
-        <div className="middle-grid-item jada-picture">
-          <img src={jadaPic} alt="Jada Lilleboe" className="img-fluid"/>
-        </div>
-        {/* <div className="right-grid=item">
-          <img src="https://via.placeholder.com/150" alt="placeholder" />
-        </div> */}
+    <div className="home-content">
+      <div className="home-left left-grid-item">
+        <h1>Hi! I'm Jada Lilleboe.</h1>
+        <h2>I'm a computer science student at the University of Minnesota Twin-Cities.</h2>
+        <p>Feel free to explore this site to learn more about me!</p>
       </div>
+      <div className="middle-grid-item jada-picture">
+        <img src={jadaPic} alt="Jada Lilleboe" className="img-fluid"/>
+      </div>
+      {/* <div className="right-grid=item">
+        <img src="https://via.placeholder.com/150" alt="placeholder" />
+      </div> */}
     </div>
   )
 }
@@ -43,6 +46,7 @@ const Footer = () => {
 const App = () => {
   return(
     <div className="content-wrapper">
+      <Navigation />
       <Home />
       <Footer />
     </div>
