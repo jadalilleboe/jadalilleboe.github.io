d4b1e3eb001246d01c87ecbfec33a334734e08d5
Jada Lilleboe <lilleboejada@gmail.com>
2021-06-08 10:12:13 -0500

    adds styling to homepage

--- src/App.js
+++ src/App.js
@@ -10,14 +10,23 @@ const Home = () => {
       <nav>
         <ul>
           <li>About</li>
+          <li>Blog</li>
           <li>Projects</li>
           <li>Contact</li>
         </ul>
       </nav>
       <div className="home-content">
-        <h1 className="left-grid-item">Hi! I'm Jada Lilleboe.</h1>
-        <h2 className="left-grid-item">I'm a computer science student at the University of Minnesota Twin-Cities.</h2>
-        <img src={jadaPic} alt="Jada Lilleboe" width={300} height={400} className="right-grid-item"/>
+        <div className="home-left left-grid-item">
+          <h1>Hi! I'm Jada Lilleboe.</h1>
+          <h2>I'm a computer science student at the University of Minnesota Twin-Cities.</h2>
+          <p>Feel free to explore this site to learn more about me!</p>
+        </div>
+        <div className="middle-grid-item jada-picture">
+          <img src={jadaPic} alt="Jada Lilleboe" className="img-fluid"/>
+        </div>
+        {/* <div className="right-grid=item">
+          <img src="https://via.placeholder.com/150" alt="placeholder" />
+        </div> */}
       </div>
     </div>
   )
