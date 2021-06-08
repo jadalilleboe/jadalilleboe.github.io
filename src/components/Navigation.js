772115923209c48fe19c2655dc8bb92cb5207d50
Jada Lilleboe <lilleboejada@gmail.com>
2021-06-08 15:30:00 -0500

    add hover styling to the link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,11 +5,11 @@ const Navigation = () => {
     return (
       <nav>
         <ul>
-          <li><NavLink to='/'>Home</NavLink></li>
-          <li><NavLink to='/about'>About</NavLink></li>
-          <li><NavLink to='/blog'>Blog</NavLink></li>
-          <li><NavLink to='/projects'>Projects</NavLink></li>
-          <li><NavLink to='/contact'>Contact</NavLink></li>
+          <li><NavLink to='/' className="navig-link">Home</NavLink></li>
+          <li><NavLink to='/about' className="navig-link">About</NavLink></li>
+          <li><NavLink to='/blog' className="navig-link">Blog</NavLink></li>
+          <li><NavLink to='/projects' className="navig-link">Projects</NavLink></li>
+          <li><NavLink to='/contact' className="navig-link">Contact</NavLink></li>
         </ul>
       </nav>
     )
