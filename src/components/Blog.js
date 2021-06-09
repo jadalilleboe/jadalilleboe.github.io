849f25fb1a841c6982497ec9481fa839c3ef0548
Jada Lilleboe <lilleboejada@gmail.com>
2021-06-09 10:42:07 -0500

    working on projects page, add css grid and coloring of the buttons

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Blog = () => {
     return (
-        <h1>Coming Soon!</h1>
+        <div className="blog-content">
+            <h1 style={{textAlign: 'center'}}>Coming Soon!</h1>
+        </div>
     )
 }
 
