63dc4db840d13b7028fe349f5c8e845e4d3b6f32
Jada Lilleboe <lilleboejada@gmail.com>
2021-06-09 13:22:43 -0500

    adds reponsive socials bar in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { SocialIcon } from 'react-social-icons';
 
 const Footer = () => {
     return (
       <footer>
         <p className="copyright">© 2021 by Jada Lilleboe.</p>
+        <ul className="socials-bar">
+          <li><SocialIcon url="https://www.linkedin.com/in/jada-lilleboe-aa2599202/" /></li>
+          <li><SocialIcon url="https://github.com/jadalilleboe" bgColor="white"/></li>
+          <li><SocialIcon url="https://www.tiktok.com/@codeforbeginners?lang=en" bgColor="white"/></li>
+        </ul>
       </footer>
     )
   }
