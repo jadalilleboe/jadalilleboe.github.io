62af2874cd46b1139855eb5fc11b2745f5521418
Jada Lilleboe <lilleboejada@gmail.com>
2021-06-09 12:24:56 -0500

    finish up project page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button, Card } from 'react-bootstrap'
 import emailPic from '../img/email.png'
+import apiPic from '../img/survivorapi.png'
 
 const ProjectCard = ({ img, title, text, ghLink, otherLink, otherLinkText }) => {
     return (
@@ -22,9 +23,9 @@ const Projects = () => {
         <div className="projects-content">
             <h1 style={{padding: 20}}>Projects</h1>
             <div className="projects-cards">
-                <ProjectCard className="project-1" img={emailPic} title="Subreddit Email Summaries" text="Lorem Ipsum Dolor" ghLink="https://github.com/jadalilleboe/subreddit_summaries" otherLink="http://subredditemailsummary.com" otherLinkText="Subreddit Email Summary Signup Website"/>
-                <ProjectCard className="project-2" img="https://via.placeholder.com/150x75" title="Survivor Database & API" text="Lorem Ipsum Dolor" otherLinkText="Postman Collection"/>
-                <ProjectCard className="project-3" img="https://via.placeholder.com/150x75" title="Example" text="Lorem Ipsum Dolor" otherLinkText="Example Website"/>
+                <ProjectCard className="project-1" img={emailPic} title="Subreddit Email Summaries" text="This is a full-stack web application that allows users to sign-up for a daily, weekly, or monthly subreddit summary sent via e-mail." ghLink="https://github.com/jadalilleboe/subreddit_summaries" otherLink="http://subredditemailsummary.com" otherLinkText="Subreddit Email Summary Signup Website"/>
+                <ProjectCard className="project-2" img={apiPic} title="Survivor Database & API" text="For this project, I used web scraping tools to make a database about CBS's show Survivor. I then built an API that could query the data." ghLink="https://github.com/jadalilleboe/survivor-db-api" otherLink="https://www.postman.com/jlillebo/workspace/survivor-api/overview" otherLinkText="Postman Collection"/>
+                <ProjectCard className="project-3" img="https://via.placeholder.com/150x75" title="Wellness Wendell" text="Work in progress. The idea is to make a web app where users can customize wellness messages sent to to them via text." otherLinkText=""/>
             </div>
         </div>
     )
