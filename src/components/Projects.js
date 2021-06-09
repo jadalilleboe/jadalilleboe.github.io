900f2481843ac34d41ded94d62b806e70a4c4b0c
Jada Lilleboe <lilleboejada@gmail.com>
2021-06-09 10:05:05 -0500

    working on projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,12 +4,13 @@ import emailPic from '../img/email.png'
 
 const ProjectCard = ({ img, title, text, ghLink, otherLink, otherLinkText }) => {
     return (
-        <Card style={{width: '30rem', color: 'white', backgroundColor: '#126948'}}>
+        <Card style={{maxWidth: '30rem', minWidth: 100, color: 'white', backgroundColor: '#126948', margin: "10px 10px 10px 100px"}}>
             <Card.Img src={img}/>
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <Card.Text>{text}</Card.Text>
                 <a href={ghLink} target="_blank" rel="noreferrer"><Button style={{backgroundColor: '#d2b48c ', color: 'black', marginBottom: 10}}>GitHub Repository</Button></a>
+                <br />
                 <a href={otherLink} target="_blank" rel="noreferrer"><Button style={{backgroundColor: '#d2b48c ', color: 'black'}}>{otherLinkText}</Button></a>
             </Card.Body>
         </Card>
@@ -19,8 +20,12 @@ const ProjectCard = ({ img, title, text, ghLink, otherLink, otherLinkText }) => 
 const Projects = () => {
     return (
         <div className="projects-content">
-            <h1>Projects</h1>
-            <ProjectCard img={emailPic} title="Subreddit Email Summaries" text="Lorem Ipsum Dolor" ghLink="https://github.com/jadalilleboe/subreddit_summaries" otherLink="http://subredditemailsummary.com" otherLinkText="Subreddit Email Summary Signup Website"/>
+            <h1 style={{padding: 20}}>Projects</h1>
+            <div className="projects-cards">
+                <ProjectCard className="project-1" img={emailPic} title="Subreddit Email Summaries" text="Lorem Ipsum Dolor" ghLink="https://github.com/jadalilleboe/subreddit_summaries" otherLink="http://subredditemailsummary.com" otherLinkText="Subreddit Email Summary Signup Website"/>
+                <ProjectCard className="project-2" img="https://via.placeholder.com/150x75" title="Example" text="Lorem Ipsum Dolor" otherLinkText="Example Website"/>
+                <ProjectCard className="project-2" img="https://via.placeholder.com/150x75" title="Example" text="Lorem Ipsum Dolor" otherLinkText="Example Website"/>
+            </div>
         </div>
     )
 }
