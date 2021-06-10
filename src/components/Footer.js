5df0a5446c180ff5e99d52b17c92e97c9fab9364
Jada Lilleboe <lilleboejada@gmail.com>
2021-06-10 09:49:46 -0500

    change link for linkedin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <footer>
         <p className="copyright">© 2021 by Jada Lilleboe.</p>
         <ul className="socials-bar">
-          <li><SocialIcon url="https://www.linkedin.com/in/jada-lilleboe-aa2599202/" target="_blank"/></li>
+          <li><SocialIcon url="https://www.linkedin.com/in/jadalilleboe/" target="_blank"/></li>
           <li><SocialIcon url="https://github.com/jadalilleboe" bgColor="white" target="_blank"/></li>
           <li><SocialIcon url="https://www.tiktok.com/@codeforbeginners?lang=en" bgColor="white" target="_blank"/></li>
         </ul>
