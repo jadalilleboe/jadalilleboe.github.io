c84d2e6cb6fc08852dce6c4abf89a480659585d1
Jada Lilleboe <lilleboejada@gmail.com>
2021-06-20 08:01:53 -0500

    change back to remote server

--- src/services/posts.js
+++ src/services/posts.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = '/api/posts'
+const baseUrl = 'https://secure-tundra-10442.herokuapp.com/api/posts'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
