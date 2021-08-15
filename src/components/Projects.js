e0b85aed4a16e1f2caa4e0ed09a2f3d4dcc06842
Jada Lilleboe <lilleboejada@gmail.com>
2021-08-15 16:30:17 -0500

    add wellness wendell project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import { Button, Card } from 'react-bootstrap'
 import emailPic from '../img/email.png'
 import apiPic from '../img/survivorapi.png'
 import hackPic from '../img/PMHackSS.png'
+import ww from '../img/ww.png'
 
 const ProjectCard = ({ img, title, text, firstLink, firstLinkText, otherLink, otherLinkText }) => {
     const cardStyle = {
@@ -34,9 +35,10 @@ const Projects = () => {
         <div className="projects-content">
             <h1 style={{padding: 20}}>Projects</h1>
             <div className="projects-cards">
-                <ProjectCard className="project-1" img={emailPic} title="Subreddit Email Summaries" text="This is a full-stack web application that allows users to sign-up for a daily, weekly, or monthly subreddit summary sent via e-mail." firstLink="https://github.com/jadalilleboe/subreddit_summaries" firstLinkText="Github Repository" otherLink="http://subredditemailsummary.com" otherLinkText="Subreddit Email Summary Signup Website"/>
-                <ProjectCard className="project-2" img={apiPic} title="Survivor Database & API" text="For this project, I used web scraping tools to make a database about CBS's show Survivor. I then built an API that could query the data." firstLink="https://github.com/jadalilleboe/survivor-db-api" firstLinkText="Github Repository" otherLink="https://www.postman.com/jlillebo/workspace/survivor-api/overview" otherLinkText="Postman Collection"/>
-                <ProjectCard className="project-3" img={hackPic} title="News Data Visualization" text="Built for the Postman Visualize for the Prize Hackathon. I visualized data from the top trending news stories with Bootstrap, Javascript, and AnyChart.js." firstLink="https://www.postman.com/jadalilleboe/workspace/postman-visualization-hackathon/overview" firstLinkText="Postman Collection" otherLink="https://youtu.be/eN0ocW3htmc" otherLinkText="Demo Video"/>
+                <ProjectCard className="project-1" img={ww} title="Wellness Wendell" text="A full stack web application that allows users to sign up for an account and customize Wellness Wendell messages sent via text." firstLink="https://github.com/jadalilleboe/wellness-wendell" firstLinkText="Github Repository" otherLink="https://wellnesswendell.herokuapp.com/#/" otherLinkText="Wellness Wendell Website"/>
+                <ProjectCard className="project-2" img={emailPic} title="Subreddit Email Summaries" text="This is a full-stack web application that allows users to sign-up for a daily, weekly, or monthly subreddit summary sent via e-mail." firstLink="https://github.com/jadalilleboe/subreddit_summaries" firstLinkText="Github Repository" otherLink="http://subredditemailsummary.com" otherLinkText="Subreddit Email Summary Signup Website"/>
+                <ProjectCard className="project-3" img={apiPic} title="Survivor Database & API" text="For this project, I used web scraping tools to make a database about CBS's show Survivor. I then built an API that could query the data." firstLink="https://github.com/jadalilleboe/survivor-db-api" firstLinkText="Github Repository" otherLink="https://www.postman.com/jlillebo/workspace/survivor-api/overview" otherLinkText="Postman Collection"/>
+                <ProjectCard className="project-4" img={hackPic} title="News Data Visualization" text="Built for the Postman Visualize for the Prize Hackathon. I visualized data from the top trending news stories with Bootstrap, JavaScript, and AnyChart.js." firstLink="https://www.postman.com/jadalilleboe/workspace/postman-visualization-hackathon/overview" firstLinkText="Postman Collection" otherLink="https://youtu.be/eN0ocW3htmc" otherLinkText="Demo Video"/>
             </div>
         </div>
     )
